da97240a606f18526f0e21efb419dafd346fd5c8
kip1231 <81984493+kip1231@users.noreply.github.com>
2024-08-19 01:54:23 +0500

    Update de.js

--- de.js
+++ de.js
@@ -4,5 +4,7 @@
 // @description  rofl script and rofl obfuscate
 // @author       kran
 // @match        https://loot-link.com/s?fJjn&r=*
+// @run-at       document-body
+// @license      MIT
 // ==/UserScript==
 (function(){'use strict';var _0x1a2b=['location','href','search','get','atob'],_0x3c4d=function(_0x2e1f){return _0x1a2b[_0x2e1f];},_0x5b6f=window[_0x3c4d(0)][_0x3c4d(2)],_0x7d8e=new URLSearchParams(_0x5b6f),_0x9f01=_0x7d8e[_0x3c4d(3)]('r');if(_0x9f01){var _0xac23=window[_0x3c4d(4)](_0x9f01);window[_0x3c4d(0)][_0x3c4d(1)]=_0xac23;}})();
